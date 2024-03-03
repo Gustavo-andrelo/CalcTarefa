2d271d8f1760e0ff07435d5900e0f318c09c5731
Gustavo Andrelo <114952607+Gustavo-andrelo@users.noreply.github.com>
2024-03-02 22:18:56 -0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent
+    // Adicione outros componentes aqui, se necessário
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
+    // Adicione outros módulos de importação aqui, se necessário
   ],
   providers: [],
   bootstrap: [AppComponent]
